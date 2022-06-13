738442a8d01b438356e2003893bab2a2341ed458
jenna-graham <jclayto2@gmail.com>
2022-06-13 14:22:55 -0700

    added images to page using City component and got image with ID

--- src/App.js
+++ src/App.js
@@ -11,16 +11,21 @@ import City from './City';
 function App() {
   // track some state here.
   // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
-  const [skylineId, setSkylineId] = useState('1');
-  const [waterfrontId, setWaterfrontId] = useState('1');
-  const [castleId, setCastleId] = useState('1');
+  const [skylineId, setSkylineId] = useState(1);
+  const [waterfrontId, setWaterfrontId] = useState(1);
+  const [castleId, setCastleId] = useState(1);
   // you'll need to track a city name, which starts as the city name of your choice.
-  const [cityName, setCityName] = useState('Portland');
+  const [cityName, setCityName] = useState('Your City Name');
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
 
   return (
     <div className="App">
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
+      <City 
+        skylineId={skylineId}
+        waterfrontId={waterfrontId}
+        castleId={castleId}
+      />
       
       <h1>
         {/* dynamically update the city name here using state */}
